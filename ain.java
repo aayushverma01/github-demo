6c370b2ad4c3625947e1de781af8167b4fcc31a7
aayushverma01 <vaayush854@gmail.com>
2025-09-09 12:45:41 +0530

    Add files via upload

---
+++ ain.java
@@ -0,0 +1,10 @@
+
+public class main{
+    public static void main(String[] args) {
+        //variables
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
